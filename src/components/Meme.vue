888e67db7f86e9ef26043319cc6fdba73d2c5050
Fabio Campioni <fabcampioni@gmail.com>
2018-06-06 17:57:49 -0400

    Initial commit

---
+++ src/components/Meme.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <img :src="url"/>
+        <h2>{{caption}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Meme',
+
+    props: {
+        url: String,
+        caption: String
+    }
+}
+</script>
+
+<style>
+
+</style>
