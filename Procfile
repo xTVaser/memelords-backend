1b3a60897e7879769c5677a658347a446c98de08
Tyler Wilding <xtvaser@gmail.com>
2018-06-16 14:41:19 -0400

    test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/memelords-backend.jar clojure.main -m backend.core
--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject backend "0.1.0"
   :description "Memelords Backend"
   :url ""
+  :min-lein-version "2.8.0"
+  :uberjar-name "memelords-backend.jar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[aleph "0.4.6"]
